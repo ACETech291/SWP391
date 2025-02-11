7fb7c721a2d623e7c10fed54cf07dfab59f0cbba
phudinhduong <166799994+phudinhduong@users.noreply.github.com>
2025-02-11 17:19:49 +0700

    Add otp database
    from Bá Hỉn

--- database/Train_Buying_Ticket_Create.ddl.sql
+++ database/Train_Buying_Ticket_Create.ddl.sql
@@ -196,3 +196,20 @@ INSERT INTO `customer` (name_customer, email_customer, password_customer, phone_
 INSERT INTO `customer` (name_customer, email_customer, password_customer, phone_number_customer, id_role, status_customer) VALUES ('Nguyễn Hữu Hiệp','admin@gmail.com','Gzy3BSM1BTMz8WpR7DvTP17/IxU=','0123456789',3,1); 
 INSERT INTO `customer` (name_customer, email_customer, password_customer, phone_number_customer, id_role, status_customer) VALUES ('phu','phu1@gmail.com','6NWFIsI1V5KNFeeazNcq35qxRUE=','0373016402',3,1); 
 INSERT INTO `customer` (name_customer, email_customer, password_customer, phone_number_customer, id_role, status_customer) VALUES ('phu123','phu123@gmail.com','6NWFIsI1V5KNFeeazNcq35qxRUE=','0373016403',3,1); 
+
+CREATE TABLE tokenForgetPassword (
+id INT AUTO_INCREMENT PRIMARY KEY,
+token VARCHAR(255) NOT NULL,
+expiryTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+isUsed BOOLEAN NOT NULL,
+userId INT NOT NULL,
+FOREIGN KEY (userId) REFERENCES customer(id_customer)
+);
+
+CREATE TABLE otp (
+id INT AUTO_INCREMENT PRIMARY KEY,
+code_otp VARCHAR(255) NOT NULL,
+expiryTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+isUsed BOOLEAN NOT NULL,
+email VARCHAR(255) NOT NULL
+);
