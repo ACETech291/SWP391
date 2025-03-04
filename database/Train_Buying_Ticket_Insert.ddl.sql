8ba89e271048bd605f1a89a64385f777f734f11c
phudinhduong <166799994+phudinhduong@users.noreply.github.com>
2025-03-04 15:56:32 +0700

    Update Train_Buying_Ticket_Insert.ddl.sql

--- database/Train_Buying_Ticket_Insert.ddl.sql
+++ database/Train_Buying_Ticket_Insert.ddl.sql
@@ -1,7 +1,4 @@
 USE SWP391;
--- role 
-USE SWP391;
-USE SWP391;
 
 -- role 
 INSERT INTO `role` (id_role, name_role) 
@@ -197,7 +194,7 @@ INSERT INTO Date_trip (id_trip,id_date_of_trip) VALUES
 (61,3),(62,3),(63,3),(64,3),(65,3),(66,3),(67,3),(68,3),(69,3),(70,3),(71,3),(72,3),
 
 (1,4),(2,4),(3,4),(4,4),(5,4),(6,4),(7,4),(8,4),(9,4),(10,4),(11,4),(12,4),(13,4),(14,4),(15,4),(16,4),(17,4),(18,4),(19,4),(20,4),
-(21,4),(22,4),(23,4),(24,4),(25,4),(26,4),(27,4),(28,4),(29,4),(30,4)(31,4),(32,4),(33,4),(34,4),(35,4),(36,4),(37,4),(38,4),(39,4),(40,4),
+(21,4),(22,4),(23,4),(24,4),(25,4),(26,4),(27,4),(28,4),(29,4),(30,4),(31,4),(32,4),(33,4),(34,4),(35,4),(36,4),(37,4),(38,4),(39,4),(40,4),
 (41,4),(42,4),(43,4),(44,4),(45,4),(46,4),(47,4),(48,4),(49,4),(50,4),(51,4),(52,4),(53,4),(54,4),(55,4),(56,4),(57,4),(58,4),(59,4),(60,4),
 (61,4),(62,4),(63,4),(64,4),(65,4),(66,4),(67,4),(68,4),(69,4),(70,4),(71,4),(72,4),
 
@@ -470,7 +467,6 @@ UPDATE `station` SET `image_station` = '../SWP391/images/stations/bienhoa.jpg' W
 UPDATE `station` SET `image_station` = '../SWP391/images/stations/saigon.jpg' WHERE `id_station` = 38;
 
 INSERT INTO `customer` (name_customer, email_customer, password_customer, phone_number_customer, id_role, status_customer) VALUES ('Nguyễn Hữu Hiệp','hiepgacute1989@gmail.com','KOfLlsfqVZEOkuIN4BdPektNF9w=','0966755095',3,1); 
-<<<<<<< Updated upstream
 INSERT INTO `Train` (name_train, image_train, description_train, id_train_brand, id_status) VALUES ('21431432', NULL, '32432432', 0, 2);
 
 INSERT INTO trip (price_trip, id__time_station_start, id_time_station_end, id_train) VALUES (0, 1, 3, 1);
